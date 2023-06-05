64ddc46893b37c2620ad4bc09d8294f5dc7b41aa
LuciaRyan <lcryan06@gmail.com>
2023-06-05 23:10:56 +0200

    added getter and setter to CreditCard class

--- src/main/java/CreditCard.java
+++ src/main/java/CreditCard.java
@@ -2,7 +2,26 @@ public class CreditCard {
     private int debt;
 
 
+    public int getDebt() {
+        return debt;
+    }
+
+    public void setDebt(int newDebt) {
+        this.debt = newDebt;
+    }
+
+
     public static void main(String[] args) {
     }
 
 }
+//  // Getter
+//  public String getName() {
+//    return name;
+//  }
+//
+//  // Setter
+//  public void setName(String newName) {
+//    this.name = newName;
+//  }
+//}
