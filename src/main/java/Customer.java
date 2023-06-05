4efff99cb0f4007588c2188bb648e9ff47724561
LuciaRyan <lcryan06@gmail.com>
2023-06-05 22:23:46 +0200

    started assignment1

---
+++ src/main/java/Customer.java
@@ -0,0 +1,12 @@
+public class Customer {
+    String name;
+    String lastName;
+    int CustomerNumber;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        Customer customer = new Customer("Lucia");
+        System.out.println(customer.name);
+        Customer customer2 = new Customer("Lionel");
+        System.out.println(customer2.name);
     }
 }
