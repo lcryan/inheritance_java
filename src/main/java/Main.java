ed23cccb5630ab5334ac981b45a3ba705e29c4b5
LuciaRyan <lcryan06@gmail.com>
2023-06-08 14:03:39 +0200

    finalised assignment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Customer customer = new Customer("Audrey", "Hepburn", 123456, new CreditCard(200));
-        Customer customer2 = new Customer("Lionel", "Richie", 123467, new CreditCard(300));
+        Customer customer = new Customer("Audrey", "Hepburn", 123456, new MasterCard(200));
+        Customer customer2 = new Customer("Lionel", "Richie", 123467, new VisaCard(400));
         customer.printName();
         customer2.printName();
-        System.out.println(customer.getCreditCard().getDebt());
-        customer.getCreditCard().pay(200);
-        System.out.println(customer.getCreditCard().getDebt());
+        System.out.println(customer2.getCreditCard().getDebt());
+        customer2.getCreditCard().pay(100);
+        System.out.println(customer2.getCreditCard().getDebt());
 
     }
 }
